b175b1d827a3dfd392fcd9c36065fe7fd17a63cd
Nick Mardon <nickmardondigital@gmail.com>
2020-07-06 17:51:23 -0700

    Employee constructor function working, 7/17 tests passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Employee class
+function Employee(name,id,email){
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.getId = function getId(){
+        console.log(this.id);
+        return this.id;
+    }
+    this.getName = function getName(){
+        console.log(this.name);
+        return this.name;
+    };
+    this.getEmail = function getEmail(){
+        console.log(this.email);
+        return this.email;
+    }
+}
+
+module.exports = Employee;
