9b8650b18c8e45b8644e1e77d4a761f035875ecc
Nick Mardon <nickmardondigital@gmail.com>
2020-07-06 18:19:31 -0700

    refactored employee constructor to use classes, 8/17 tests passing

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,19 +1,24 @@
 // TODO: Write code to define and export the Employee class
-function Employee(name,id,email){
+class Employee{
+constructor (name,id,email){
     this.name = name;
     this.id = id;
     this.email = email;
-    this.getId = function getId(){
-        console.log(this.id);
-        return this.id;
     }
-    this.getName = function getName(){
-        console.log(this.name);
-        return this.name;
-    };
-    this.getEmail = function getEmail(){
-        console.log(this.email);
-        return this.email;
+    getName() {
+        return this.name
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return "Employee"
     }
 }
 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+function Engineer(){
+    this.github = githubURL;
+    this.getRole = Engineer;
+    this.getGithub = function getGithub(){
+        console.log(this.github);
+        return this.github;
+    }
+}
+
+var kurt = new Engineer(kurtBurtbrent);
+kurt.getGithub;
+
+
+module.exports = Engineer;
