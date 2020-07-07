57d58ba13bef33370423f7559fd3cbe8a3d84ca8
Nick Mardon <nickmardondigital@gmail.com>
2020-07-06 18:55:54 -0700

    Passing Engineer test suite 11/17 overall

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,17 +2,30 @@ const Employee = require("./Employee");
 
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
-function Engineer(){
-    this.github = githubURL;
-    this.getRole = Engineer;
-    this.getGithub = function getGithub(){
-        console.log(this.github);
+class Engineer extends Employee {
+    constructor(name,id,email,github){
+        super(name,id,email);
+        this.github = github;
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole(){
+        return "Engineer";
+    }
+    getGithub(){
         return this.github;
     }
 }
-
-var kurt = new Engineer(kurtBurtbrent);
-kurt.getGithub;
-
 
 module.exports = Engineer;
